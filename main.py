64d908082cc11d3ad7da0136d5ffc837ac33db30
rholoshenko2-prog <rholoshenko2@gmail.com>
2026-02-12 18:03:26 +0300

    Добавлен минимальный рабочий код запуска бота на aiogram
    Added asynchronous Telegram bot setup with aiogram.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Price Tracker started!")
-# Пока просто заглушка — дальше будем добавлять парсер и бота
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import TELEGRAM_TOKEN
+
+async def main():
+    bot = Bot(token=TELEGRAM_TOKEN)
+    dp = Dispatcher()
+
+    print("Бот запущен и готов к работе!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
