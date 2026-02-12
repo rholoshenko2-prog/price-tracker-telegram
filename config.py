63c344e1cf3176f9592c5441fe2c132c8aead3a2
rholoshenko2-prog <rholoshenko2@gmail.com>
2026-02-12 17:31:13 +0300

    git add config.py git commit -m "Настроен config.py с чтением .env" git push

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-# Сюда положим токен бота, ID чата, ссылки на товары и т.д.
-TELEGRAM_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # загружает .env
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = int(os.getenv("CHAT_ID", "0"))  # если нет — 0
+
+# Позже сюда добавим ссылки на товары, интервал проверки и т.д.
